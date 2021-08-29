aa5cd87e94cf59d4603b2c4ca091581e576546e9
moqi <moqimoqidea@gmail.com>
2021-08-29 16:12:33 +0800

    Update TestThreadLocalNoInheritable

--- chapter2/src/main/java/com/concurrent/program/thread/base/TestThreadLocalNoInheritable.java
+++ chapter2/src/main/java/com/concurrent/program/thread/base/TestThreadLocalNoInheritable.java
@@ -4,25 +4,26 @@ package com.concurrent.program.thread.base;
  * Created on 2020-08-29
  */
 public class TestThreadLocalNoInheritable {
+
     // (1) 创建线程变量
-    public static ThreadLocal<String> threadLocal = new ThreadLocal<String>();
+    public static ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
+    /**
+     * main:hello world
+     * thread:null
+     */
     public static void main(String[] args) {
-
         // (2)  设置线程变量
         threadLocal.set("hello world");
         // (3) 启动子线程
-        Thread thread = new Thread(new Runnable() {
-            public void run() {
-                // (4)子线程输出线程变量的值
-                System.out.println("thread:" + threadLocal.get());
-
-            }
+        Thread thread = new Thread(() -> {
+            // (4)子线程输出线程变量的值
+            System.out.println("thread:" + threadLocal.get());
         });
         thread.start();
 
         // (5)主线程输出线程变量值
         System.out.println("main:" + threadLocal.get());
-
     }
+
 }
