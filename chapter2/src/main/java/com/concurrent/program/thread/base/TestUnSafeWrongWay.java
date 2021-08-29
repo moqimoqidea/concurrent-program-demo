7482da621f09c4f15138ec09b17475054d0b83e4
moqi <moqimoqidea@gmail.com>
2021-08-29 17:13:23 +0800

    Update TestUnSafeWrongWay

--- chapter2/src/main/java/com/concurrent/program/thread/base/TestUnSafeWrongWay.java
+++ chapter2/src/main/java/com/concurrent/program/thread/base/TestUnSafeWrongWay.java
@@ -5,17 +5,17 @@ import sun.misc.Unsafe;
 /**
  * Created on 2020-08-29
  */
-public class TestUnSafeWrong {
+public class TestUnSafeWrongWay {
 
     // 获取Unsafe的实例（2.2.1）
-    static final Unsafe unsafe = UnsafeUtils.getUnsafe();
+    static final Unsafe unsafe = Unsafe.getUnsafe();
     // 记录变量state在类TestUnSafe中的偏移值（2.2.2）
     static final long stateOffset;
 
     static {
         try {
             // 获取state变量在类TestUnSafe中的偏移值(2.2.4)
-            stateOffset = unsafe.objectFieldOffset(TestUnSafeWrong.class.getDeclaredField("state"));
+            stateOffset = unsafe.objectFieldOffset(TestUnSafeWrongWay.class.getDeclaredField("state"));
             System.out.println("stateOffset = " + stateOffset);
         } catch (Exception ex) {
             System.out.println(ex.getLocalizedMessage());
@@ -27,12 +27,14 @@ public class TestUnSafeWrong {
     private final long state = 0;
 
     /**
-     * stateOffset = 16
-     * true
+     * Exception in thread "main" java.lang.ExceptionInInitializerError
+     * Caused by: java.lang.SecurityException: Unsafe
+     * 	at sun.misc.Unsafe.getUnsafe(Unsafe.java:90)
+     * 	at com.concurrent.program.thread.base.TestUnSafeWrong.<clinit>(TestUnSafeWrong.java:11)
      */
     public static void main(String[] args) {
         // 创建实例，并且设置state值为1(2.2.5)
-        TestUnSafeWrong test = new TestUnSafeWrong();
+        TestUnSafeWrongWay test = new TestUnSafeWrongWay();
         // (2.2.6)
         Boolean success = unsafe.compareAndSwapInt(test, stateOffset, 0, 1);
         System.out.println(success);
