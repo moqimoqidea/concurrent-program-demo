e801f8878ec6e90804033ae23b7a52391b0f8fdd
moqi <moqimoqidea@gmail.com>
2021-08-29 18:13:02 +0800

    Update Atomic

--- chapter4/src/main/java/com/concurrent/program/atomic/Atomic.java
+++ chapter4/src/main/java/com/concurrent/program/atomic/Atomic.java
@@ -7,42 +7,29 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class Atomic {
     // (10)创建Long型原子计数器
-    private static AtomicLong atomicLong = new AtomicLong();
+    private static final AtomicLong atomicLong = new AtomicLong();
     // (11)创建数据源
-    private static Integer[] arrayOne = new Integer[] {0, 1, 2, 3, 0, 5, 6, 0, 56, 0};
-    private static Integer[] arrayTwo = new Integer[] {10, 1, 2, 3, 0, 5, 6, 0, 56, 0};
+    private static final Integer[] arrayOne = new Integer[] {0, 1, 2, 3, 0, 5, 6, 0, 56, 0};
+    private static final Integer[] arrayTwo = new Integer[] {10, 1, 2, 3, 0, 5, 6, 0, 56, 0};
 
+    /**
+     * count 0 occurs: 7
+     */
     public static void main(String[] args) throws InterruptedException {
         // （12）线程one统计数组arrayOne中0的个数
-        Thread threadOne = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-
-                int size = arrayOne.length;
-                for (int i = 0; i < size; ++i) {
-                    if (arrayOne[i].intValue() == 0) {
-
-                        atomicLong.incrementAndGet();
-                    }
+        Thread threadOne = new Thread(() -> {
+            for (Integer integer : arrayOne) {
+                if (integer == 0) {
+                    atomicLong.incrementAndGet();
                 }
-
             }
         });
         // （13）线程two统计数组arrayTwo中0的个数
-        Thread threadTwo = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-
-                int size = arrayTwo.length;
-                for (int i = 0; i < size; ++i) {
-                    if (arrayTwo[i].intValue() == 0) {
-
-                        atomicLong.incrementAndGet();
-                    }
+        Thread threadTwo = new Thread(() -> {
+            for (Integer integer : arrayTwo) {
+                if (integer == 0) {
+                    atomicLong.incrementAndGet();
                 }
-
             }
         });
 
@@ -54,7 +41,7 @@ public class Atomic {
         threadOne.join();
         threadTwo.join();
 
-        System.out.println("count 0:" + atomicLong.get());
+        System.out.println("count 0 occurs: " + atomicLong.get());
 
     }
 }
