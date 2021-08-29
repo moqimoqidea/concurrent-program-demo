0221b49b42632d7cfb82ef57b8bf7fe54421768d
moqi <moqimoqidea@gmail.com>
2021-08-29 15:27:40 +0800

    Update InterruptFlagTest2

--- chapter2/src/main/java/com/concurrent/program/thread/base/InterruptFlagTest2.java
+++ chapter2/src/main/java/com/concurrent/program/thread/base/InterruptFlagTest2.java
@@ -4,19 +4,21 @@ package com.concurrent.program.thread.base;
  * Created on 2020-08-29
  */
 public class InterruptFlagTest2 {
+
+    /**
+     * threadOne isInterrupted:false
+     * main thread is over
+     */
     public static void main(String[] args) throws InterruptedException {
-
         // 1.创建线程
-        Thread threadOne = new Thread(new Runnable() {
-            public void run() {
+        Thread threadOne = new Thread(() -> {
 
-                // 中断标志为true时候会退出循环，并且清除中断标志
-                while (!Thread.currentThread().interrupted()) {
+            // 中断标志为true时候会退出循环，并且清除中断标志
+            while (!Thread.currentThread().interrupted()) {
 
-                }
-
-                System.out.println("threadOne isInterrupted:" + Thread.currentThread().isInterrupted());
             }
+
+            System.out.println("threadOne isInterrupted:" + Thread.currentThread().isInterrupted());
         });
 
         // 2. 启动线程
@@ -28,6 +30,6 @@ public class InterruptFlagTest2 {
         // 3.等待线程one退出
         threadOne.join();
         System.out.println("main thread is over");
-
     }
+
 }
