a03058e00fd515007b5ce06ad7cfc4ab34831bb4
moqi <moqimoqidea@gmail.com>
2021-08-29 15:11:05 +0800

    Update InterruptedStopTest

--- chapter2/src/main/java/com/concurrent/program/thread/base/InterruptedStopTest.java
+++ chapter2/src/main/java/com/concurrent/program/thread/base/InterruptedStopTest.java
@@ -8,20 +8,18 @@ public class InterruptedStopTest {
     public static void main(String[] args) throws InterruptedException {
 
         // 1.创建线程
-        Thread threadOne = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    System.out.println("threadOne begin sleep for 2000 seconds");
-                    Thread.sleep(2000000);
-                    System.out.println("threadOne awaking");
+        Thread threadOne = new Thread(() -> {
+            try {
+                System.out.println("threadOne begin sleep for 2000 seconds");
+                Thread.sleep(2000000);
+                System.out.println("threadOne awaking");
 
-                } catch (InterruptedException e) {
-                    System.out.println("threadOne is interrupted while sleeping");
-                    return;
-                }
-
-                System.out.println("threadOne-leaving normally");
+            } catch (InterruptedException e) {
+                System.out.println("threadOne is interrupted while sleeping");
+                return;
             }
+
+            System.out.println("threadOne-leaving normally");
         });
 
         // 2.启动线程
@@ -36,6 +34,6 @@ public class InterruptedStopTest {
         threadOne.join();
 
         System.out.println("main thread is over");
-
     }
+
 }
