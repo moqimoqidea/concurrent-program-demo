b7ad0f1c6e40534065fb600a614794aea1248fda
moqi <moqimoqidea@gmail.com>
2021-08-29 12:14:22 +0800

    Update

--- chapter2/src/main/java/com/concurrent/program/thread/base/YieldTest.java
+++ chapter2/src/main/java/com/concurrent/program/thread/base/YieldTest.java
@@ -1,10 +1,27 @@
 package com.concurrent.program.thread.base;
 
 /**
- * * Created on 2020-08-29
+ * Created on 2020-08-29
  */
 public class YieldTest implements Runnable {
 
+    /**
+     * yield close:
+     * Thread[Thread-0,5,main]yield cpu...
+     * Thread[Thread-2,5,main]yield cpu...
+     * Thread[Thread-2,5,main] is over
+     * Thread[Thread-1,5,main]yield cpu...
+     * Thread[Thread-1,5,main] is over
+     * Thread[Thread-0,5,main] is over
+     * <p>
+     * yield open:
+     * Thread[Thread-0,5,main]yield cpu...
+     * Thread[Thread-1,5,main]yield cpu...
+     * Thread[Thread-2,5,main]yield cpu...
+     * Thread[Thread-1,5,main] is over
+     * Thread[Thread-0,5,main] is over
+     * Thread[Thread-2,5,main] is over
+     */
     public static void main(String[] args) {
         //创建并启动线程
         new Thread(new YieldTest()).start();
@@ -16,7 +33,7 @@ public class YieldTest implements Runnable {
 
         for (int i = 0; i < 5; i++) {
             //当i=0时候出让cpu执行权，放弃时间片，进行下一轮调度
-            if ((i % 5) == 0) {
+            if (i == 0) {
                 System.out.println(Thread.currentThread() + "yield cpu...");
                 //当前 出让cpu执行权，放弃时间片，进行下一轮调度
                 Thread.yield();
