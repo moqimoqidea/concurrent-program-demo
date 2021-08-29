3238b9ec2df73493e1d7feb41c07941a4136b1b2
moqi <moqimoqidea@gmail.com>
2021-08-29 10:18:43 +0800

    Update

--- chapter1/src/test/groovy/DoubleLockTest.groovy
+++ chapter1/src/test/groovy/DoubleLockTest.groovy
@@ -1,16 +1,20 @@
 //file:noinspection GroovySynchronizationOnNonFinalField
+
 import groovy.util.logging.Slf4j
 import spock.lang.Ignore
 import spock.lang.Specification
 
+import java.util.concurrent.TimeUnit
+
 @Slf4j
-@Ignore
 class DoubleLockTest extends Specification {
 
     private static volatile Object resourceA = new Object()
     private static volatile Object resourceB = new Object()
 
     // stay in: threadB try get resourceB lock
+    // 当线程调用共享对象的 wait() 方法时，当前线程只会释放当前共享对象的锁，当前线程持有的其他共享对象的监视器锁并不会被释放。
+    @Ignore
     def 'test two lock execute path'() {
         when:
         Thread threadA = new Thread(() -> {
@@ -30,6 +34,7 @@ class DoubleLockTest extends Specification {
 
         Thread threadB = new Thread(() -> {
             try {
+                TimeUnit.SECONDS.sleep(1)
                 synchronized (resourceA) {
                     log.info('threadB get resourceA lock')
                     log.info('threadB try get resourceB lock')
