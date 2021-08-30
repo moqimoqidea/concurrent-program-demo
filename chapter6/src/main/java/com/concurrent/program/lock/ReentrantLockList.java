594154dbbe817dfb914e98007f60ac7b7bb28de8
moqi <moqimoqidea@gmail.com>
2021-08-30 10:34:21 +0800

    Update files

--- chapter6/src/main/java/com/concurrent/program/lock/ReentrantLockList.java
+++ chapter6/src/main/java/com/concurrent/program/lock/ReentrantLockList.java
@@ -7,6 +7,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * Created on 2020-08-29
  */
 public class ReentrantLockList {
+
     // 线程不安全的list
     private final ArrayList<String> array = new ArrayList<>();
     // 独占锁
@@ -14,11 +15,9 @@ public class ReentrantLockList {
 
     // 添加元素
     public void add(String e) {
-
         lock.lock();
         try {
             array.add(e);
-
         } finally {
             lock.unlock();
 
@@ -27,27 +26,23 @@ public class ReentrantLockList {
 
     // 删元素
     public void remove(String e) {
-
         lock.lock();
         try {
             array.remove(e);
 
         } finally {
             lock.unlock();
-
         }
     }
 
     // 获取数据
     public String get(int index) {
-
         lock.lock();
         try {
             return array.get(index);
-
         } finally {
             lock.unlock();
-
         }
     }
+
 }
--- chapter6/src/main/java/com/concurrent/program/lock/ReentrantReadWriteLockList.java
+++ chapter6/src/main/java/com/concurrent/program/lock/ReentrantReadWriteLockList.java
@@ -2,16 +2,15 @@ package com.concurrent.program.lock;
 
 import java.util.ArrayList;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Created on 2020-08-29
  */
-public class ReentrantLockList2 {
+public class ReentrantReadWriteLockList {
 
     // 线程不安全的list
-    private ArrayList<String> array = new ArrayList<String>();
+    private ArrayList<String> array = new ArrayList<>();
     // 独占锁
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
     private final Lock readLock = lock.readLock();
@@ -19,40 +18,32 @@ public class ReentrantLockList2 {
 
     // 添加元素
     public void add(String e) {
-
         writeLock.lock();
         try {
             array.add(e);
-
         } finally {
             writeLock.unlock();
-
         }
     }
 
     // 删元素
     public void remove(String e) {
-
         writeLock.lock();
         try {
             array.remove(e);
-
         } finally {
             writeLock.unlock();
-
         }
     }
 
     // 获取数据
     public String get(int index) {
-
         readLock.lock();
         try {
             return array.get(index);
 
         } finally {
             readLock.unlock();
-
         }
     }
 
